cb35a87107b52bc1f255f2ef06136fdaf33290fb
Václav Bolom <bolom@centrum.cz>
2024-07-30 15:40:10 +0200

    add communication with port

--- CardTester/SerialPortOperator.cs
+++ CardTester/SerialPortOperator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace CardTester
 {
@@ -20,14 +21,35 @@ namespace CardTester
       private const string STOP = "0";
 
       private bool _CancelOperation = false;
-   
-      public async Task Run()
+    
+      private string _PortName = string.Empty;
+
+      private string _LastState = string.Empty;
+
+      private string _Command = string.Empty;
+      public SerialPortOperator(string portName)
+      {
+        _PortName = portName;
+      }
+
+        public async Task Run()
       {
         while (!_CancelOperation)
         {
           //open port
+          var port = OpenPort();
+
           //read state
+          var response = await port.ReadAsync(5);
+          _LastState = System.Text.Encoding.UTF8.GetString(response);
+
           // write command if there is command to write
+          if (_Command != string.Empty)
+          {
+            port.WriteLine(_Command);
+            _Command = string.Empty;
+          }
+          port.Close();
           await Task.Delay(1000);
         }
       }
@@ -47,6 +69,18 @@ namespace CardTester
       {
         return string.Empty;
       }
+
+    private SerialPort OpenPort()
+    {
+			var port = new SerialPort
+			{
+				PortName = _PortName,
+				BaudRate = 9600,
+				ReadTimeout = 10
+			};
+
+      return port;
+		}
 
 	}
 }
