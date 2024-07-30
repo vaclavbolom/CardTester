ec62202bc534a2ca437fd20675ab9283f0d1f56f
Václav Bolom <bolom@centrum.cz>
2024-07-30 15:40:23 +0200

    initial commit

---
+++ CardTester/SerialPortExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace CardTester
+{
+	public static class SerialPortExtensions
+	{
+		public async static Task ReadAsync(this SerialPort serialPort, byte[] buffer, int offset, int count)
+		{
+			var bytesToRead = count;
+			var temp = new byte[count];
+
+			while (bytesToRead > 0)
+			{
+				var readBytes = await serialPort.BaseStream.ReadAsync(temp, 0, bytesToRead);
+				Array.Copy(temp, 0, buffer, offset + count - bytesToRead, readBytes);
+				bytesToRead -= readBytes;
+			}
+		}
+
+		public async static Task<byte[]> ReadAsync(this SerialPort serialPort, int count)
+		{
+			var buffer = new byte[count];
+			await serialPort.ReadAsync(buffer, 0, count);
+			return buffer;
+		}
+	}
+}
