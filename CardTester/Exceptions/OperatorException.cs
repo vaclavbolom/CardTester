8ceab57ec882b2428b079d00843ae396d9d35cbc
Václav Bolom <bolom@centrum.cz>
2024-07-30 15:38:50 +0200

    initial commit

---
+++ CardTester/Exceptions/OperatorException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTester.Exceptions
+{
+	public class OperatorException : Exception
+	{
+		public OperatorException() { }
+
+		public OperatorException(string message) : base(message)
+		{
+		}
+
+		public OperatorException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
