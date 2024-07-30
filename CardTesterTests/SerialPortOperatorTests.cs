60dce203607092218c7cba300e8cd7c8359992e1
Václav Bolom <bolom@centrum.cz>
2024-07-30 17:04:00 +0200

    create tests

---
+++ CardTesterTests/SerialPortOperatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CardTester;
+
+namespace CardTesterTests
+{
+	public class SerialPortOperatorTests
+	{
+		[Fact]
+		public void SerialPortOperatorTest()
+		{
+			Assert.True(true);
+		}
+
+		[Fact]
+		public async void Run_Default_Expected()
+		{
+			var portOperator = new SerialPortOperator("COM4");
+
+			await portOperator.Run();
+			Assert.NotNull(portOperator);
+
+			//TODO: test operator states
+		}
+	}
+}
