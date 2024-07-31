6bc302be0aaa4e14d00653511f21f45fa60a01c4
Václav Bolom <bolom@centrum.cz>
2024-07-31 14:41:47 +0200

    create testing INO

--- Arduino/serial_test/serial_test.ino
+++ Arduino/serial_test/serial_test.ino
@@ -1,9 +1,22 @@
+
+const int N = 3;
+const int DELAY = 100;
+String commands[N] = {"A", "B", "C"};
+int i = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(commands[i]);
 
+  if (i == (N - 1))
+    i = 0;
+  else
+    i++;
+
+  delay(DELAY);
 }
