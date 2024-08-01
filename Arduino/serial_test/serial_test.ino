f10075d288ec76ed71832f172365ed6f550c0089
Václav Bolom <bolom@centrum.cz>
2024-08-01 22:41:21 +0200

    update test INO

--- Arduino/serial_test/serial_test.ino
+++ Arduino/serial_test/serial_test.ino
@@ -1,6 +1,6 @@
 
 const int N = 3;
-const int DELAY = 1000;
+const int DELAY = 100;
 String commands[N] = {"A", "B", "C"};
 int i = 0;
 
@@ -11,7 +11,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(commands[i]);
+  Serial.println(commands[i]);
 
   if (i == (N - 1))
     i = 0;
