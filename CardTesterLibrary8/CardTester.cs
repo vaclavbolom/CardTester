7c499750d331601cb4e7987c1d1f4d84cb824f65
Václav Bolom <bolom@centrum.cz>
2024-08-02 06:55:28 +0200

    create interface and clas

---
+++ CardTesterLibrary8/CardTester.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardTester;
+using Microsoft.Extensions.Logging;
+
+namespace CardTesterLibrary
+{
+    public class CardTester : ICardTester
+    {
+        private const string STATE_FORWARD = "f";
+        private const string STATE_BACKWARD = "b";
+        private const string STATE_UP = "u";
+        private const string STATE_DOWN = "d";
+        private const string STATE_STOPPED = "s";
+        private const string STATE_UNKNOWN = "x";
+
+        private const string MOVE_FORWARD = "1";
+        private const string MOVE_BACKWARD = "2";
+        private const string STOP = "0";
+        private const string RESET = "3";
+
+
+        private readonly ILogger _Logger;
+        private readonly ISerialPortOperator _serialPortOperator;
+
+        private string _State;
+
+        public CardTester(ILogger logger, ISerialPortOperator serialPortOperator)
+        {
+            ArgumentNullException.ThrowIfNull(logger, nameof(logger));
+            ArgumentNullException.ThrowIfNull(serialPortOperator, nameof(serialPortOperator));
+            _Logger = logger;
+            _serialPortOperator = serialPortOperator;
+            _State = STATE_UNKNOWN;
+        }
+
+        public Task MoveDownAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RunTestAsync(int numberOfCycles, int delayUp, int delayDown)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task MoveForwardAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task MoveBackwardAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task MeasureAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CardTesterLibrary8/ICardTester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    /// <summary>
+    /// Methods for testing smart cards in bending machine.
+    /// </summary>
+    public interface ICardTester
+    {
+        /// <summary>
+        /// Runs smart card test
+        /// </summary>
+        /// <param name="numberOfCycles">Number of bending cycles</param>
+        /// <param name="delayUp">Delay in upper position before card test</param>
+        /// <param name="delayDown">delay in lower position before card test</param>
+        /// <returns></returns>
+        public Task RunTestAsync(int numberOfCycles, int delayUp, int delayDown);
+
+        /// <summary>
+        /// Moves piston to lower position in case it is stopped.
+        /// </summary>
+        /// <returns></returns>
+        public Task MoveDownAsync();
+
+        /// <summary>
+        /// Stops piston movement.
+        /// </summary>
+        /// <returns></returns>
+        public Task StopAsync();
+    }
+}
