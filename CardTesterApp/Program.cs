06f42658af478a87dcd355a581a0f2223baf138f
Václav Bolom <bolom@centrum.cz>
2024-08-27 22:41:20 +0200

    inject services into form

--- CardTesterApp/Program.cs
+++ CardTesterApp/Program.cs
@@ -1,3 +1,7 @@
+using CardTesterLibrary;
+using Serilog;
+using Serilog.Events;
+
 namespace CardTesterApp
 {
 	internal static class Program
@@ -8,10 +12,20 @@ namespace CardTesterApp
 		[STAThread]
 		static void Main()
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			Application.Run(new TestingApp());
+            Log.Logger = new LoggerConfiguration()
+              .WriteTo.File("CardTesterApp.log", LogEventLevel.Debug)
+              .MinimumLevel.Debug()
+              .CreateLogger();
+            var logger = Log.Logger;
+			logger.Debug("Application started");
+
+			var serialPortOperator = new SerialPortOperator("COM4", logger);
+			var cardTester = new CardTester(logger, serialPortOperator);
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+			Application.Run(new TestingAppForm(cardTester, logger));
 		}
 	}
 }
