0791d549ff3cb08ee45e2f10d4022d0027ab9583
Václav Bolom <bolom@centrum.cz>
2024-08-29 21:26:19 +0200

    fix typo

---
+++ CardTesterLibrary8/Models/ApplicationSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    public  class ApplicationSettings
+    {
+        public string PortName { get; set; } = string.Empty;
+
+        public string OutputDirectory { get; set; } = string.Empty;
+
+        public int NumberOfCycles { get; set; }
+
+        public decimal DelayBasic { get; set; }
+
+        public decimal DelayBent { get; set; }
+    }
+}
