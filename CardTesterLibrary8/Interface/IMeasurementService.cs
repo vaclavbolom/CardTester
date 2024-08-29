41c543a0057e406e6e5303c8b9d768e506fa46df
Václav Bolom <bolom@centrum.cz>
2024-08-29 07:45:06 +0200

    create service

---
+++ CardTesterLibrary8/Interface/IMeasurementService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    /// <summary>
+    /// Service for measurement of cards
+    /// </summary>
+    public interface IMeasurementService
+    {
+        /// <summary>
+        /// Measures card response
+        /// </summary>
+        /// <returns>result of measurement</returns>
+        public Measurement MeasureCards();
+    }
+}
---
+++ CardTesterLibrary8/Services/MeasurementServiceMock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace CardTesterLibrary
+{
+    /// <inheritdoc/>
+    public class MeasurementServiceMock : IMeasurementService
+    {
+        private readonly ILogger _logger;
+        public MeasurementServiceMock(ILogger logger)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(logger));
+            _logger = logger;
+        }
+        public Measurement MeasureCards()
+        {
+            var result = new Measurement
+            {
+                Timestamp = DateTime.Now,
+                Card1 = "OK",
+                Card2 = "OK",
+                Card3 = "OK",
+                Card4 = "OK"
+            };
+
+            _logger.Debug("Cards measured");
+            return result;
+        }
+    }
+}
