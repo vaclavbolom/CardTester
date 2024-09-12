c6412f4c9cf40d8a0d98525e77a162666924d14a
Vaclav BOLOM <vaclav.bolom@cz.abb.com>
2024-09-12 20:22:56 +0200

    create exception

---
+++ CardTesterLibrary8/Exceptions/CardTesterException.cs
@@ -0,0 +1,27 @@
+﻿using Serilog.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    public class CardTesterException : Exception
+    {
+        public CardTesterException()
+        {
+            
+        }
+
+        public CardTesterException(string? message) : base(message)
+        {
+            
+        }
+
+        public CardTesterException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
---
+++ CardTesterLibrary8/Exceptions/SerialPortOperatorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    public class SerialPOrtOperatorException : Exception
+    {
+        public SerialPOrtOperatorException()
+        {
+        }
+
+        public SerialPOrtOperatorException(string? message) : base(message)
+        {
+        }
+
+        public SerialPOrtOperatorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }     
+    }
+}
