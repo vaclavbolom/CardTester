4e00a4fcab0cbee68d0136baed67365ba31f6a3b
Václav Bolom <bolom@centrum.cz>
2024-10-01 18:11:25 +0200

    create exception

---
+++ CardTesterLibrary8/Exceptions/MeasurementServiceException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTesterLibrary
+{
+    /// <summary>
+    /// Exception for Measuremnt service
+    /// </summary>
+    public class MeasurementServiceException : Exception
+    {
+        public MeasurementServiceException() { }
+
+        public MeasurementServiceException(string? message) : base(message)
+        {
+        }
+
+        public MeasurementServiceException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+        
+        
+    }
+}
