db8cf00f4f18ddb71b0df52aef88828ed0f6a476
Václav Bolom <bolom@centrum.cz>
2024-10-01 18:11:14 +0200

    update model

--- CardTesterLibrary8/Models/Measurement.cs
+++ CardTesterLibrary8/Models/Measurement.cs
@@ -19,28 +19,50 @@ namespace CardTesterLibrary
         /// <summary>
         /// Card 1 result
         /// </summary>
-        public string? Card1 { get; set; }
+        public string? Card1Result { get; set; }
 
         /// <summary>
         /// Card2 result
         /// </summary>
-        public string? Card2 { get; set; }
+        public string? Card2Result { get; set; }
 
         /// <summary>
         /// Card 3 result
         /// </summary>
-        public string? Card3 { get; set; }
+        public string? Card3Result { get; set; }
 
         /// <summary>
         /// Card 4 result
         /// </summary>
-        public string? Card4 { get; set; }
+        public string? Card4Result { get; set; }
+
+        /// <summary>
+        /// Card 1 Note
+        /// </summary>
+        public string? Card1Note { get; set; }
+
+        /// <summary>
+        /// Card2 Note
+        /// </summary>
+        public string? Card2Note { get; set; }
+
+        /// <summary>
+        /// Card 3 Note
+        /// </summary>
+        public string? Card3Note { get; set; }
+
+        /// <summary>
+        /// Card 4 Note
+        /// </summary>
+        public string? Card4Note { get; set; }
 
         /// <summary>
         /// Position of bending machine
         /// expected BASIC/BENT
         /// </summary>
         public string? Position { get; set; } = string.Empty;
+
+        public int MeasurementIndex { get; set; }
 
     }
 }
